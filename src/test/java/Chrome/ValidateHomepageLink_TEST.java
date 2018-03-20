d76257049b27bd739c513bc96d0e3f9c75d88d38
gdhadi <igede.nurhadi@gmail.com>
2018-03-21 01:49:45 +0800

    optimize before method, by added get, manage and import PO

--- src/test/java/Chrome/ValidateHomepageLink_TEST.java
+++ src/test/java/Chrome/ValidateHomepageLink_TEST.java
@@ -1,46 +1,40 @@
 package Chrome;
 
 import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
 import com.stagingm.com.stagingm.test.HomePage_PO;
 
 public class ValidateHomepageLink_TEST {
+	public static WebDriver driver;
+	public static HomePage_PO homepage;
 	
 @BeforeMethod
 	public void intiateDriver (){
-		System.setProperty ("webdriver.chrome.driver","C:\\WebDriver\\chromedriver.exe");
+	System.setProperty ("webdriver.chrome.driver","C:\\WebDriver\\chromedriver.exe");
+	driver = new ChromeDriver();
+	driver.get("https://stagingmitrais.com/en/");
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+	homepage = new HomePage_PO(driver);
 	}; 
 	
 @Test(priority=1)
     public void aboutUsLink ()throws InterruptedException {
-	
-	ChromeDriver driver = new ChromeDriver();
-	driver.get("https://stagingmitrais.com/en/");
-	driver.manage().window().maximize();
-	driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-	
-	HomePage_PO homepage = new HomePage_PO(driver);
 	homepage.CLICKaboutUsLink();
 	homepage.ASSERTaboutUsLink();
-    
-	//close browser
+    //close browser
 	driver.close();
 	
 	};
 @Test(priority=2)
     public void ServiceSofDevLink (){
-	
-	ChromeDriver driver = new ChromeDriver();
-	driver.get("https://stagingmitrais.com/en/");
-	driver.manage().window().maximize();
-	driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-	
-	HomePage_PO homepage = new HomePage_PO(driver);
 	homepage.HOVERservice();
 	homepage.ASSERTserviceSofDev();
-
 	//close browser
 	driver.close();
     };
